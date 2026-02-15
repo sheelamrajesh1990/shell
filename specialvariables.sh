edbabadb1d098599706daae5a27b2103bef3dee2
sheelamrajesh1990 <sheelamrajeshkumar0409@gmail.com>
2026-02-15 10:42:56 +0530

    actions

--- specialvariables.sh
+++ specialvariables.sh
@@ -3,7 +3,7 @@
 echo "All args passed to script $@"
 echo "Number of vars passed to script $#"
 echo "Script name $0"
-echo "Present directory $PWD
+echo "Present directory $PWD"
 echo "Who is running $USER"
 echo "Home directory of current user $HOME"
 echo "PID of this script $$"
