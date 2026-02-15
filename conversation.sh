7a015061f6e85d5722323e705899e8c40e8fc7e2
sheelamrajesh1990 <sheelamrajeshkumar0409@gmail.com>
2026-02-15 09:52:42 +0530

    actions

--- conversation.sh
+++ conversation.sh
@@ -2,6 +2,6 @@
 PERSON1=Rajesh
 PERSON2=Mahesh
 
-echo "$PERSON1 : Hello Mahesh How are you"
-echo "$PERSON2 : I am fine"
-echo "$PERSON1 : Good"
+echo "$PERSON1 : Hello $PERSON2 How are you"
+echo "$PERSON2 : I am fine" $PERSON1
+echo "$PERSON1 : Good" $PERSON2
