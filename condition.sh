233d4f1e940eb5507e1c0f11019ef56e2aaea61b
sheelamrajesh1990 <sheelamrajeshkumar0409@gmail.com>
2026-02-15 11:25:21 +0530

    actions

--- condition.sh
+++ condition.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-NUMBER=$25
+NUMBER=$1
 # -gt - greater than
 # -lt - less than
 # -eq - equals
 # -ne - not equal
-if [$NUMBER -g 20]; then
+if [ $NUMBER -g 20 ]; then
 echo "Given number $NUMBER  is greater than 20"
+else 
+echo "Given number $NUMBER  is less than 20"
 fi  
